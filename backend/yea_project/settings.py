b74003c8aca5f5c20c8ea69f37a96a99fb673ba3
Никита Песчанов <86157460+ItWasCain@users.noreply.github.com>
2026-05-17 22:47:10 +0400

    Update settings.py

--- backend/yea_project/settings.py
+++ backend/yea_project/settings.py
@@ -155,7 +155,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
